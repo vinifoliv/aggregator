b72c7aef229de96b11388b04633427f8db46101f
Vinicius <vinicius.ferreira22615@gmail.com>
2024-05-30 22:25:24 -0300

    feat: creates a .txt file with the most information of a particular article from the feed

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const Parser = require('rss-parser');
-const parser = new Parser();
+const file_system = require('fs');
+const { stringify } = require('querystring');
+const parser = new (require('rss-parser'))();
 
 (async () => {
     let feed = await parser.parseURL('https://www.reddit.com/.rss');
-    console.log(feed.title);
+    let cont = 1;
 
+    // Logging to the terminal
+    console.log(feed.title + '\n\n');
     feed.items.forEach(item => {
-        console.log(item.title + ':' + item.link)
+        console.log('Item ' + cont + ' - ' + item.title);
+        cont++;
+    });
+
+    // Saving the article's title
+    let article_index = 0;
+
+    let article = stringify({
+        title: feed.title,
+        date: feed.items.at(article_index).date,
+        headline: feed.items.at(article_index).title,
+        link: feed.items.at(article_index).link,
+    });
+
+    file_system.writeFileSync('./articles.txt', article, (error) => {
+        if (error) {
+            console.log('Error: ' + error);
+        }
+        else {
+            console.log('Success!');
+        }
     });
 })();
